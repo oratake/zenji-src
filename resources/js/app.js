495b1a6158787aa9d7e4ab306af1982cada19240
oratake <smith.shimomura@gmail.com>
2021-09-15 00:25:28 +0900

    test: aboutページ追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,9 +14,18 @@ require('./bootstrap');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Example from './components/Example';
+import { BrowserRouter, Route } from 'react-router-dom';
+import About from './pages/About';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Route path="/about" component={About} />
+    </BrowserRouter>
+  );
+}
 
 if (document.getElementById('app')) {
-  ReactDOM.render(<Example />, document.getElementById('app'));
+  ReactDOM.render(<App />, document.getElementById('app'));
 }
 
