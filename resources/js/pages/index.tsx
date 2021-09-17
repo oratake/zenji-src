eb29f936bee52670e7d755939bc22bb520c6ec57
oratake <smith.shimomura@gmail.com>
2021-09-17 23:07:06 +0900

    feat: リンク設置テスト

--- resources/js/pages/index.tsx
+++ resources/js/pages/index.tsx
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Index = () => {
   return (
-    <></>
+    <>
+      <div>index</div>
+      <div><Link to="/dashboard">link to dashboard</Link></div>
+    </>
   );
 }
 
